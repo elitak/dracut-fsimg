a5df7eddb105ebb16053811b027d6a7e61114695
Eric Litak <elitak@gmail.com>
2014-06-24 00:23:25 -0700

    this condition is needed in order to boot without fsimg params defined (for vm flat-file boot)

--- parse-fsimg.sh
+++ parse-fsimg.sh
@@ -3,7 +3,7 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 #if [ "${root%%:*}" = "fsimg" ] ; then
-if true; then
+if [ -n "$fspart" ]; then
     modprobe ntfs
     fspart=$(getarg rd.fspart)
     wait_for_dev $fspart
